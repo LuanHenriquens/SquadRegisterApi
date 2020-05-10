9899b6105777084b52eafca4c1b014e8fa2a586c
Luan Henrique <luanhenriquens@hotmail.com>
2020-05-09 23:45:52 -0300

    Add validations in Member Insert

--- Services/MemberService.cs
+++ Services/MemberService.cs
@@ -1,5 +1,7 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using SquadRegisterApi.Auxiliar;
 using SquadRegisterApi.Data;
 using SquadRegisterApi.Models;
@@ -19,6 +21,20 @@ namespace SquadRegisterApi.Services
         {
             try
             {
+                if (string.IsNullOrEmpty(member.name))
+                    throw new ValidationException("The name property cannot be null or empty");
+
+                if (string.IsNullOrEmpty(member.function))
+                    throw new ValidationException("The function property cannot be null or empty");
+
+                if(string.IsNullOrEmpty(member.squad_id.ToString()) || member.squad_id <= 0)
+                    throw new ValidationException("The squad_id property cannot be null, empty or 0.");
+                
+                var squad = await this._context.Squad.FirstOrDefaultAsync(c => c.id == member.squad_id);
+
+                if(squad == null)
+                    throw new ValidationException("Invalid squad.");
+
                 member.create_date = DateTime.Now;
 
                 await this._context.Member.AddAsync(member);
@@ -28,7 +44,7 @@ namespace SquadRegisterApi.Services
             }
             catch (Exception ex)
             {
-                
+
                 throw new Exception(ex.GetInnerException());
             }
         }
